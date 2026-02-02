d22be502e22af74e19c333cf84ae4acbde13c0ef
besselink_r <besselink_r@ca01.ub.lan>
2026-02-02 13:23:39 +0800

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,27 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Welcome react</h1>
+      <Card cardTitle="First Card"/>
+      <Card cardTitle="Second Card"/>
+      <Card cardTitle="Third Card"/>
+    </>
+  )
+}
+
+const Card = ({cardTitle}) => {
+  const [like, setLike] = useState(false)
+  return (
+    <>
+    <div>
+    <h2>This is a card {cardTitle}</h2>
+    <button onClick ={() => setLike(!like)}>
+      {like ? 'Unlike' : 'Like'}
+    </button>
+    </div>
     </>
   )
 }
